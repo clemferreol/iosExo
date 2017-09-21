ac7613605a7fe59697834a64374170956f90220c
clemferreol <cleol@9online.fr>
2017-09-21 14:42:26 +0200

    Initial Commit

---
+++ project1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  project1
+//
+//  Created by Clementine Ferreol on 21/09/2017.
+//  Copyright © 2017 Clementine Ferreol. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
